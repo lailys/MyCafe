20e06214373410cda820168261533048292d1f68
LailyS <laily@lailys.com>
2019-10-08 16:13:43 -0700

    add all the files

---
+++ build/precache-manifest.20e16140e3a0e2e0371d75c57576f6ce.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "3b47da581ca4f0229945",
+    "url": "/static/js/main.3b47da58.chunk.js"
+  },
+  {
+    "revision": "9150b6079341f6cdbe51",
+    "url": "/static/js/1.9150b607.chunk.js"
+  },
+  {
+    "revision": "3b47da581ca4f0229945",
+    "url": "/static/css/main.16fab9c5.chunk.css"
+  },
+  {
+    "revision": "e6c7412d19195522de943e210222162f",
+    "url": "/index.html"
+  }
+];
